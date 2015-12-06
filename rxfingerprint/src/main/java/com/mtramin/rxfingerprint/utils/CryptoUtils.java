2e077fd0bf34a224d6d45857d57547a12d767d5c
Mauin <marvin.ramin@gmail.com>
2015-12-06 18:26:16 +0100

    javadocs for cryptoutils

--- rxfingerprint/src/main/java/com/mtramin/rxfingerprint/utils/CryptoUtils.java
+++ rxfingerprint/src/main/java/com/mtramin/rxfingerprint/utils/CryptoUtils.java
@@ -20,28 +20,39 @@ import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 
 /**
- * TODO: JAVADOC
+ * Utility methods to access cryptographic entities for RxFingerprint.
  */
 public class CryptoUtils {
 
-    /**
-     * Alias for our key in the Android Key Store
-     */
     // TODO make this user changeable
     private static final String DEFAULT_KEY_NAME = "rxfingerprint_default";
     private static final int DEFAULT_KEY_SIZE = 256;
 
-    public static Cipher createCipher() throws NoSuchPaddingException, NoSuchAlgorithmException {
-        return Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/"
-                + KeyProperties.BLOCK_MODE_CBC + "/"
-                + KeyProperties.ENCRYPTION_PADDING_PKCS7);
-    }
-
+    /**
+     * @return Initialized cipher for encryption operations in RxFingerprint
+     */
     public static Cipher initEncryptionCipher() throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, NoSuchProviderException, InvalidAlgorithmParameterException {
         Cipher cipher = createCipher();
         SecretKey key = createKey();
         cipher.init(Cipher.ENCRYPT_MODE, key);
         return cipher;
+    }
+
+    /**
+     * @param iv initialization vector used during encryption
+     * @return Initialized cipher for decryption operations in RxFingerprint
+     */
+    public static Cipher initDecryptionCipher(byte[] iv) throws CertificateException, NoSuchAlgorithmException, IOException, InvalidKeyException, UnrecoverableKeyException, KeyStoreException, InvalidAlgorithmParameterException, NoSuchPaddingException {
+        Cipher cipher = createCipher();
+        SecretKey key = getKey();
+        cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
+        return cipher;
+    }
+
+    private static Cipher createCipher() throws NoSuchPaddingException, NoSuchAlgorithmException {
+        return Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/"
+                + KeyProperties.BLOCK_MODE_CBC + "/"
+                + KeyProperties.ENCRYPTION_PADDING_PKCS7);
     }
 
     private static SecretKey createKey() throws NoSuchProviderException, NoSuchAlgorithmException, InvalidAlgorithmParameterException {
@@ -54,13 +65,6 @@ public class CryptoUtils {
                 .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
                 .build());
         return keyGenerator.generateKey();
-    }
-
-    public static Cipher initDecryptionCipher(byte[] iv) throws CertificateException, NoSuchAlgorithmException, IOException, InvalidKeyException, UnrecoverableKeyException, KeyStoreException, InvalidAlgorithmParameterException, NoSuchPaddingException {
-        Cipher cipher = createCipher();
-        SecretKey key = getKey();
-        cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
-        return cipher;
     }
 
     private static SecretKey getKey() throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException, UnrecoverableKeyException {

1c7ce04ec80fdf6c2cd011ab224813fb4f84c630
Marvin Ramin <marvin.ramin@gmail.com>
2017-02-06 20:06:15 +0100

    reformmat CryptoProvider

--- rxfingerprint/src/main/java/com/mtramin/rxfingerprint/CryptoProvider.java
+++ rxfingerprint/src/main/java/com/mtramin/rxfingerprint/CryptoProvider.java
@@ -49,32 +49,32 @@ import javax.crypto.spec.IvParameterSpec;
  */
 class CryptoProvider {
 
-    private static final String ANDROID_KEY_STORE = "AndroidKeyStore";
-    private static final String DEFAULT_KEY_NAME = "rxfingerprint_default";
-    private static final int DEFAULT_KEY_SIZE = 256;
+	private static final String ANDROID_KEY_STORE = "AndroidKeyStore";
+	private static final String DEFAULT_KEY_NAME = "rxfingerprint_default";
+	private static final int DEFAULT_KEY_SIZE = 256;
 
-    private final String keyName;
+	private final String keyName;
 
-    /**
-     * Creates a new CryptoProvider. If a keyName is provided, uses the given key in the KeyStore
-     * for cryptographic operations. The given key name is {@code null} a default key name will be
-     * used.
-     * <p/>
-     * The default key name will consist of the applications package name appended with
-     * {@link CryptoProvider#DEFAULT_KEY_NAME}.
-     *
-     * @param context context to use, may not be null
-     * @param keyName keyName to use, can be null
-     */
-    CryptoProvider(@NonNull Context context, @Nullable String keyName) {
-        if (keyName == null) {
-            this.keyName = ContextUtils.getPackageName(context) + "." + DEFAULT_KEY_NAME;
-        } else {
-            this.keyName = keyName;
-        }
-    }
+	/**
+	 * Creates a new CryptoProvider. If a keyName is provided, uses the given key in the KeyStore
+	 * for cryptographic operations. The given key name is {@code null} a default key name will be
+	 * used.
+	 * <p/>
+	 * The default key name will consist of the applications package name appended with
+	 * {@link CryptoProvider#DEFAULT_KEY_NAME}.
+	 *
+	 * @param context context to use, may not be null
+	 * @param keyName keyName to use, can be null
+	 */
+	CryptoProvider(@NonNull Context context, @Nullable String keyName) {
+		if (keyName == null) {
+			this.keyName = ContextUtils.getPackageName(context) + "." + DEFAULT_KEY_NAME;
+		} else {
+			this.keyName = keyName;
+		}
+	}
 
-    /**
+	/**
 	 * Gets or creates a key and initializes a cipher with it in {@link Cipher.ENCRYPT_MODE}
 	 * In case the key was permanently invalidated the key will be deleted and re-created.
 	 *
@@ -92,15 +92,15 @@ class CryptoProvider {
 	}
 
 	/**
-     * @param iv initialization vector used during encryption
-     * @return Initialized cipher for decryption operations in RxFingerprint
-     */
-    Cipher initDecryptionCipher(byte[] iv) throws CertificateException, NoSuchAlgorithmException, IOException, InvalidKeyException, UnrecoverableKeyException, KeyStoreException, InvalidAlgorithmParameterException, NoSuchPaddingException {
-        Cipher cipher = createCipher();
-        SecretKey key = getKey(keyName);
-        cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
-        return cipher;
-    }
+	 * @param iv initialization vector used during encryption
+	 * @return Initialized cipher for decryption operations in RxFingerprint
+	 */
+	Cipher initDecryptionCipher(byte[] iv) throws CertificateException, NoSuchAlgorithmException, IOException, InvalidKeyException, UnrecoverableKeyException, KeyStoreException, InvalidAlgorithmParameterException, NoSuchPaddingException {
+		Cipher cipher = createCipher();
+		SecretKey key = getKey(keyName);
+		cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
+		return cipher;
+	}
 
 	private Cipher cipherForEncryption() throws NoSuchAlgorithmException, NoSuchPaddingException, CertificateException, UnrecoverableKeyException, KeyStoreException, NoSuchProviderException, InvalidAlgorithmParameterException, IOException, InvalidKeyException {
 		Cipher cipher = createCipher();
@@ -109,58 +109,58 @@ class CryptoProvider {
 		return cipher;
 	}
 
-    private void removeKey(String keyName) throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException {
-        if (keyExists(keyName)) {
-            KeyStore keyStore = KeyStore.getInstance(ANDROID_KEY_STORE);
-            keyStore.load(null);
-            keyStore.deleteEntry(keyName);
-        }
-    }
+	private void removeKey(String keyName) throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException {
+		if (keyExists(keyName)) {
+			KeyStore keyStore = KeyStore.getInstance(ANDROID_KEY_STORE);
+			keyStore.load(null);
+			keyStore.deleteEntry(keyName);
+		}
+	}
 
-    @TargetApi(Build.VERSION_CODES.M)
-    private Cipher createCipher() throws NoSuchPaddingException, NoSuchAlgorithmException {
-        return Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/"
-                + KeyProperties.BLOCK_MODE_CBC + "/"
-                + KeyProperties.ENCRYPTION_PADDING_PKCS7);
-    }
+	@TargetApi(Build.VERSION_CODES.M)
+	private Cipher createCipher() throws NoSuchPaddingException, NoSuchAlgorithmException {
+		return Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/"
+				+ KeyProperties.BLOCK_MODE_CBC + "/"
+				+ KeyProperties.ENCRYPTION_PADDING_PKCS7);
+	}
 
-    private SecretKey findOrCreateKey(String keyName) throws NoSuchProviderException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, UnrecoverableKeyException, CertificateException, KeyStoreException, IOException {
-        if (keyExists(keyName)) {
-            return getKey(keyName);
-        }
-        return createKey(keyName);
-    }
+	private SecretKey findOrCreateKey(String keyName) throws NoSuchProviderException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, UnrecoverableKeyException, CertificateException, KeyStoreException, IOException {
+		if (keyExists(keyName)) {
+			return getKey(keyName);
+		}
+		return createKey(keyName);
+	}
 
-    private boolean keyExists(String keyName) throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException {
-        KeyStore keyStore = KeyStore.getInstance(ANDROID_KEY_STORE);
-        keyStore.load(null);
-        Enumeration<String> aliases = keyStore.aliases();
+	private boolean keyExists(String keyName) throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException {
+		KeyStore keyStore = KeyStore.getInstance(ANDROID_KEY_STORE);
+		keyStore.load(null);
+		Enumeration<String> aliases = keyStore.aliases();
 
-        while (aliases.hasMoreElements()) {
-            if (keyName.equals(aliases.nextElement())) {
-                return true;
-            }
-        }
+		while (aliases.hasMoreElements()) {
+			if (keyName.equals(aliases.nextElement())) {
+				return true;
+			}
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    @TargetApi(Build.VERSION_CODES.M)
-    private SecretKey createKey(String keyName) throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, ANDROID_KEY_STORE);
-        keyGenerator.init(new KeyGenParameterSpec.Builder(keyName,
-                KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
-                .setKeySize(DEFAULT_KEY_SIZE)
-                .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
-                .setUserAuthenticationRequired(true)
-                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
-                .build());
-        return keyGenerator.generateKey();
-    }
+	@TargetApi(Build.VERSION_CODES.M)
+	private SecretKey createKey(String keyName) throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException {
+		KeyGenerator keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, ANDROID_KEY_STORE);
+		keyGenerator.init(new KeyGenParameterSpec.Builder(keyName,
+				KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
+				.setKeySize(DEFAULT_KEY_SIZE)
+				.setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+				.setUserAuthenticationRequired(true)
+				.setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+				.build());
+		return keyGenerator.generateKey();
+	}
 
-    private SecretKey getKey(String keyName) throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException, UnrecoverableKeyException {
-        KeyStore keyStore = KeyStore.getInstance(ANDROID_KEY_STORE);
-        keyStore.load(null);
-        return (SecretKey) keyStore.getKey(keyName, null);
-    }
+	private SecretKey getKey(String keyName) throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException, UnrecoverableKeyException {
+		KeyStore keyStore = KeyStore.getInstance(ANDROID_KEY_STORE);
+		keyStore.load(null);
+		return (SecretKey) keyStore.getKey(keyName, null);
+	}
 }
